5b3e357048be3cb86d2fb0953300256d8b10503a
Rigidus <i.am.rigidus@gmail.com>
2023-03-20 11:10:48 +0300

    ffmpeg: png to gif sh script >> run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 tesseract bw.png test2 --psm 6 -l rus+eng lstmbox
 tesseract bw.png test -l rus+eng
+ffmpeg \
+  -framerate 60 \
+  -pattern_type glob \
+  -i 'tmp.*.png' \
+  -r 15 \
+  -vf scale=512:-1 \
+  out.gif \
+;
